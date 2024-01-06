a7ce78728f5cec06a9d530272eb0f158c1d7a8c6
janbuetof <jan.buetof@moia.io>
2024-01-06 14:24:18 +0100

    add dbt models

---
+++ dbt/models/intermediate/pokemon__donations__aggregated.sql
@@ -0,0 +1,11 @@
+{{ config(materialized="view") }}
+
+select 
+    pokemon_id
+    , sum(donation_number_of_pokemon) as total_number_of_pokemon_donaters
+    , round(sum(donation_amount_eur),2) as total_amount_donated_eur
+    , round(sum(donation_amount_eur)::dec / sum(donation_number_of_pokemon)::dec,2) as average_donation_amount_eur_per_pokemon_donator
+from 
+    {{ source("pokemon_load", "pokemon__donations",) }}
+group by 
+    pokemon_id
