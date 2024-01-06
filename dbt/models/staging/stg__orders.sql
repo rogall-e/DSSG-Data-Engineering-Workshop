12f451eb483546decbd3eef75ba53dfd4446db7a
janbuetof <jan.buetof@moia.io>
2024-01-06 14:24:09 +0100

    delete example

--- dbt/models/staging/stg__orders.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{source('staging','raw_orders')}}
--- dbt/models/staging/stg__payment.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id as payment_id,
-    order_id,
-    payment_method,
-    amount
-from {{source('staging','raw_payments')}}
