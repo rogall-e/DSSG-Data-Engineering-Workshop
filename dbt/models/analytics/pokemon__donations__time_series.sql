498a5c1c87319a716e1111d940a3d6806a9e94ce
jan butof <jan.buetof@moia.io>
2024-01-11 17:54:47 +0000

    delete round function

--- dbt/models/analytics/pokemon__donations__time_series.sql
+++ dbt/models/analytics/pokemon__donations__time_series.sql
@@ -1,8 +1,8 @@
 select 
     donation_date
     , sum(donation_number_of_pokemon) as total_number_of_pokemon_donaters
-    , round(sum(donation_amount_eur),2) as total_amount_donated_eur
-    , round(sum(donation_amount_eur)::dec / sum(donation_number_of_pokemon)::dec,2) as average_donation_amount_eur_per_pokemon_donator
+    , sum(donation_amount_eur) as total_amount_donated_eur
+    , sum(donation_amount_eur)::dec / sum(donation_number_of_pokemon)::dec as average_donation_amount_eur_per_pokemon_donator
 from 
     {{ source("pokemon_load", "pokemon__donations",) }}
 group by 
